7cd184eb6fd1f6c51923d0d6beb3012d2a4e74a0
Aaaaaaaa <marakuyaspb@gmail.com>
2023-02-20 22:18:54 +0300

    slader with fabric samples is okey

---
+++ template-parts/card-parts/slider-fabric-change.php
@@ -0,0 +1,45 @@
+<div class='slider-fabric'>
+
+  <div class='slide-fabric-nav'>  
+    <img class='slide-fabric-button active' id='fabric1' src="http://decona.ru/wp-content/uploads/2023/02/VElutto-16.png" onclick='changeFabric1()'> 
+    <img class='slide-fabric-button' id='fabric2' src="http://decona.ru/wp-content/uploads/2023/02/VElutto-32-copy.png" onclick='changeFabric2()'>
+    <img class='slide-fabric-button' id='fabric3' src= "http://decona.ru/wp-content/uploads/2023/02/Jazz-01-copy.png" onclick='changeFabric3()'>
+    <img class='slide-fabric-button' id='fabric4' src="http://decona.ru/wp-content/uploads/2023/02/Jazz-08-copy.png" onclick='changeFabric4()'>
+    <img class='slide-fabric-button' id='fabric5' src="http://decona.ru/wp-content/uploads/2023/02/Jazz-21-copy.png" onclick='changeFabric5()'> 
+  </div>
+
+  <div class="slides-fabric">
+    <div id='sofaFabric1'>
+      <center>
+        <p class='small'>Ткань: Jazz 01 (Кат.5)
+      </center>
+      <img src='http://decona.ru/wp-content/uploads/2023/01/Снимок-экрана-от-2023-01-20-03-19-02.png' class="img">
+    </div>
+    <div id='sofaFabric2'>
+      <center>
+        <p class='small'>Ткань: Jazz 02 (Кат.55)
+      </center>
+      <img src='http://decona.ru/wp-content/uploads/2023/01/Снимок-экрана-от-2023-01-20-03-18-05.png' class="img">
+    </div>
+    <div id='sofaFabric3'>
+      <center>
+        <p class='small'>Ткань: Jazz 03 (Кат.33)
+      </center>
+      <img src='http://decona.ru/wp-content/uploads/2023/01/Снимок-экрана-от-2023-01-20-03-18-57.png' class="img">
+    </div>
+    <div id='sofaFabric4'>
+      <center>
+        <p class='small'>Ткань: Jazz 04 (Кат.4)
+      </center>
+      <img src='http://decona.ru/wp-content/uploads/2023/01/Снимок-экрана-от-2023-01-20-03-18-14.png' class="img">
+    </div>
+    <div id='sofaFabric5'>
+      <center>
+        <p class='small'>Ткань: Jazz 05 (Кат.555)
+      </center>
+      <img src='http://decona.ru/wp-content/uploads/2023/02/Снимок-экрана-от-2023-01-20-03-18-37.png' class="img">
+    </div>
+  </div>
+
+</div>
+
