fdd90460d15d173ef585da6fcf94903e8df1bdd6
Aaaaaaaa <marakuyaspb@gmail.com>
2023-02-22 14:06:11 +0300

    optomize pop over function

--- template-parts/card-parts/popover.php
+++ template-parts/card-parts/popover.php
@@ -1,60 +1,64 @@
-<div id='portland'>
-  <img src='http://decona.ru/wp-content/uploads/2023/02/Portland_for-details-11.png' class='under-info'>
+<div class='popOverBlock'>
+  <div class='popPosition'>
+    <div id='portland'>
+      <img src='http://decona.ru/wp-content/uploads/2023/02/Portland_for-details-11.png' class='under-info'>
+    </div>
+    <div class='pins'>
+        <!-- pins -->
+        <img class='pin' id='pinStitches' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onclick='showStitches()' onmouseout='hidStitches()'> 
+        <img class='pin' id='pinPillow' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onclick='showPillow()' onmouseout='hidPillow()'> 
+        <img class='pin' id='pinFront' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onclick='showFront()' onmouseout='hidFront()'> 
+        <img class='pin' id='pinBack' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onclick='showBack()' onmouseout='hidBack()'>      
+        <img class='pin' id='pinPaw' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onclick='showPaw()' onmouseout='hidPaw()'> 
+    </div>   
+      <!-- Pop overs -->
+      <div class = 'popOvers'>
 
-    <!-- pins -->
-    <img class='pin' id='pinStitches' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onmousemove='showStitches()' onmouseout='hidStitches()'> 
-    <img class='pin' id='pinPillow' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onmousemove='showPillow()' onmouseout='hidPillow()'> 
-    <img class='pin' id='pinFront' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onmousemove='showFront()' onmouseout='hidFront()'> 
-    <img class='pin' id='pinBack' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onmousemove='showBack()' onmouseout='hidBack()'>      
-    <img class='pin' id='pinPaw' src="http://decona.ru/wp-content/uploads/2023/02/pop-point.png" onmousemove='showPaw()' onmouseout='hidPaw()'> 
-      
-    <!-- Pop overs -->
-    <div class = 'popOvers'>
-
-      <div id='popAboutStitches'>
-        <div class='card' style='width: 15rem;'>
-          <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
-          <div class='card-body'>
-            <p class='card-text'>Мы прошиваем наши диваны турецкими мебельными нитями марки POLYART. Это синтетические армированные  нити с полиэфирной оплёткой. Нити обладают высокой прочностью и устойчивы к истиранию. </p>
-          </div>
+        <div id='popAboutStitches'>
+          <div class='card' style='width: 15rem;'>
+            <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
+            <div class='card-body'>
+              <p class='card-text'>Мы прошиваем наши диваны турецкими мебельными нитями марки POLYART. Это синтетические армированные  нити с полиэфирной оплёткой. Нити обладают высокой прочностью и устойчивы к истиранию. </p>
+            </div>
+         </div>
        </div>
-     </div>
 
-     <div id='popAboutPillow'>
-        <div class='card' style='width: 15rem;'>
-          <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
-          <div class='card-body'>
-            <p class='card-text'>Подушка может лежать на диване. А может быть сброшена кошкой. Ещё ей можно драться. </p>
-          </div>
+       <div id='popAboutPillow'>
+          <div class='card' style='width: 15rem;'>
+            <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
+            <div class='card-body'>
+              <p class='card-text'>Подушка может лежать на диване. А может быть сброшена кошкой. Ещё ей можно драться. </p>
+            </div>
+         </div>
        </div>
-     </div>
 
-     <div id='popAboutFront'>
-        <div class='card' style='width: 15rem;'>
-          <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
-          <div class='card-body'>
-            <p class='card-text'>Край дивана. Наверное, здесь что-то о диванном внутреннем мире - какие там пружины внутри хитрые. Или нет? </p>
-          </div>
+       <div id='popAboutFront'>
+          <div class='card' style='width: 15rem;'>
+            <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
+            <div class='card-body'>
+              <p class='card-text'>Край дивана. Наверное, здесь что-то о диванном внутреннем мире - какие там пружины внутри хитрые. Или нет? </p>
+            </div>
+         </div>
        </div>
-     </div>
 
-     <div id='popAboutBack'>
-        <div class='card' style='width: 15rem;'>
-          <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
-          <div class='card-body'>
-            <p class='card-text'>Спинка дивана. Наверное, здесь о наполнителе - какой он белый, пушистый и высокотехнологичный. Или нет? </p>
-          </div>
+       <div id='popAboutBack'>
+          <div class='card' style='width: 15rem;'>
+            <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
+            <div class='card-body'>
+              <p class='card-text'>Спинка дивана. Наверное, здесь о наполнителе - какой он белый, пушистый и высокотехнологичный. Или нет? </p>
+            </div>
+         </div>
        </div>
-     </div>
 
-     <div id='popAboutPaw'>
-        <div class='card' style='width: 15rem;'>
-          <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
-          <div class='card-body'>
-            <p class='card-text'>Диванная ножка. Наверное, здесь о том, что она из дуба 4 сортов. Или нет? </p>
-          </div>
+       <div id='popAboutPaw'>
+          <div class='card' style='width: 15rem;'>
+            <img src='http://decona.ru/wp-content/uploads/2023/02/Detail_1-1.png' class='card-img-top'>
+            <div class='card-body'>
+              <p class='card-text'>Диванная ножка. Наверное, здесь о том, что она из дуба 4 сортов. Или нет? </p>
+            </div>
+         </div>
        </div>
-     </div>
 
+    </div>
   </div>
-      
+</div>
