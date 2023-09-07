50c470af7ebe55098f890450f5da3f15025e7302
Marakuyaspb <marakuyaspb@gmail.com>
2023-09-07 17:26:57 +0300

    added new info in accordeon

--- catalog-craft/w_j.py
+++ catalog-craft/w_j.py
@@ -3,3 +3,10 @@ import json
 def write(data, filename):
 	data = json.dumps(data)
 	data = json.load(str(data))
+	with open(filename, 'w', encoding = 'utf-8') as file:
+		json.dump(data, file, indent = 4)
+
+
+data = 'products_norm.json'
+
+write(data)
